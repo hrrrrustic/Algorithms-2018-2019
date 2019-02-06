3c0b442da1e771651554732a466529fb383e13c1
Hrrrrustic <hrrrrustic@gmail.com>
2019-02-07 01:35:04 +0300

    Components RE 13

---
+++ Components/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Components
+{
+    class Program
+    {
+        static bool[] visited;
+        static int[] vertexList;
+        static void Main(string[] args)
+        {
+            int componentNumber = 1;
+            string[] data = File.ReadAllLines("components.in");
+            int vertexCount = int.Parse(data[0].Split(' ').First());
+            int edgeCount = int.Parse(data[0].Split(' ').Last());
+            vertexList = new int[vertexCount];
+            visited = new bool[vertexCount];
+            int[,] matrix = new int[vertexCount, vertexCount];
+            for (int i = 0; i < edgeCount; i++)
+            {
+                int j = int.Parse(data[i + 1].Split(' ').First()) - 1;
+                int k = int.Parse(data[i + 1].Split(' ').Last()) - 1;
+                matrix[j, k] = 1;
+                matrix[k, j] = 1;
+            }
+            for (int i = 0; i < vertexCount; i++)
+            {
+                if(!visited[i])
+                    BFS(ref matrix, i, vertexCount, ref componentNumber);
+            }
+            using (var outFile = new StreamWriter("components.out"))
+            {
+                outFile.WriteLine(componentNumber - 1);
+                for (int i = 0; i < vertexCount; i++)
+                {
+                    outFile.Write(vertexList[i] + " ");
+                }
+            }
+        }
+        static void BFS(ref int[,] matrix, int startVertex, int vertexCount, ref int compNum)
+        {
+            Queue<int> dfsqueue = new Queue<int>();
+            dfsqueue.Enqueue(startVertex);
+            visited[startVertex] = true;
+            vertexList[startVertex] = compNum;
+            while (dfsqueue.Count != 0)
+            {
+                int curr = dfsqueue.Dequeue();
+                for (int i = startVertex; i < vertexCount; i++)
+                {
+                    if(matrix[curr, i] == 1 && visited[i] == false)
+                    {
+                        visited[i] = true;
+                        vertexList[i] = compNum;
+                        dfsqueue.Enqueue(i);
+                    }
+                }
+            }
+            compNum++;
+        }   
+    }
+}
