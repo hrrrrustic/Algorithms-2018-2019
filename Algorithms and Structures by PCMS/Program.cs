356314180259ea81f02bb6c0253d271e8d5af5bb
Hrrrrustic <hrrrrustic@gmail.com>
2019-04-20 12:59:28 +0300

    fix .csproj and CamelCase

--- Algorithms and Structures by PCMS/Program.cs
+++ Algorithms and Structures by PCMS/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Algorithms_and_Structures_by_PCMS
+namespace AlgorithmsAndStructuresByPCMS
 {
     class Program
     {
