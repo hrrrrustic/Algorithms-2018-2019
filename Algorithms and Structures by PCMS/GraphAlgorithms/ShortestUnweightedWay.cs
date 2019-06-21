4d1eb6cb050c813d444efe19c63000615979a7ca
Vlad <hrrrrustic@gmail.com>
2019-06-21 06:24:56 +0300

    [refactor]

--- Algorithms and Structures by PCMS/GraphAlgorithms/ShortestUnweightedWay.cs
+++ Algorithms and Structures by PCMS/GraphAlgorithms/ShortestUnweightedWay.cs
@@ -8,60 +8,81 @@ namespace AlgorithmsAndStructuresByPCMS.GraphAlgorithms
 
     public class BreadthFirstSearch
     {
-        static bool[] visited;
-        static int[] vertexList;
-        static List<int>[] adjList;
+        private class Graph
+        {
+            public int VertexCount { get; }
+            public List<int>[] AdjList { get; }
+            public bool[] Visited { get; }
 
+            public Graph(int vertexCount, int edgeCount, List<int>[] adjList)
+            {
+                VertexCount = vertexCount;
+                AdjList = adjList;
+                Visited = new bool[vertexCount];
+            }
+        }
         public static void Solve(string[] args)
         {
             int[][] inputData = File
                 .ReadAllLines("pathbge1.in")
                 .Select(k => k.Trim().Split(' ').Select(int.Parse).ToArray())
                 .ToArray();
-            int vertexCount = inputData[0][0];
-            int edgeCount = inputData[0][1];
-            vertexList = new int[vertexCount];
-            visited = new bool[vertexCount];
-            adjList = new List<int>[vertexCount];
-            for (int i = 0; i < edgeCount; i++)
-            {
-                string[] splittedData = inputData[i + 1].Split(' ');
-                int j = int.Parse(splittedData[0]) - 1;
-                int k = int.Parse(splittedData[1]) - 1;
-                if (adjList[j] == null)
-                    adjList[j] = new List<int>();
-                adjList[j].Add(k);
-                if (adjList[k] == null)
-                    adjList[k] = new List<int>();
-                adjList[k].Add(j);
-            }
-            BFS(0, vertexCount);
-            string answer = string.Join(" ", vertexList);
-            Console.WriteLine(answer);
+
+            Graph currentGraph = InitGraph(inputData.Skip(1).ToArray(), inputData[0][0], inputData[0][1]);
+
+            int[] distance = BFSSearchDistance(currentGraph);
+            Console.WriteLine(string.Join(" ", distance));
         }
 
-        private static void BFS(int startVertex, int vertexCount)
+        private static Graph InitGraph(int[][] data, int vertexCount, int edgeCount)
         {
-            Queue<int> dfsqueue = new Queue<int>();
-            dfsqueue.Enqueue(startVertex);
-            visited[startVertex] = true;
-            vertexList[startVertex] = 0;
-            while (dfsqueue.Count != 0)
+            List<int>[] adjList = new List<int>[vertexCount];
+            for (int i = 0; i < edgeCount; i++)
             {
-                int curr = dfsqueue.Dequeue();
-                if (adjList[curr] != null)
+                int fromVertex = data[i][0] - 1;
+                int toVertex = data[i][1] - 1;
+
+                if (adjList[fromVertex] == null)
+                    adjList[fromVertex] = new List<int>();
+                adjList[fromVertex].Add(toVertex);
+
+                if (adjList[toVertex] == null)
+                    adjList[toVertex] = new List<int>();
+                adjList[toVertex].Add(fromVertex);
+            }
+
+            return new Graph(vertexCount, edgeCount, adjList);
+        }
+        private static int[] BFSSearchDistance(Graph graph)
+        {
+            const int startVertex = 0;
+            int[] distanceFromStart = new int[graph.VertexCount];
+
+            Queue<int> queue = new Queue<int>();
+
+            queue.Enqueue(startVertex);
+            graph.Visited[startVertex] = true;
+            distanceFromStart[startVertex] = 0;
+
+            while (queue.Count != 0)
+            {
+                int currentVertex = queue.Dequeue();
+
+                if (graph.AdjList[currentVertex] != null)
                 {
-                    for (int i = 0; i < adjList[curr].Count; i++)
+                    for (int i = 0; i < graph.AdjList[currentVertex].Count; i++)
                     {
-                        if (!visited[adjList[curr][i]])
+                        if (!graph.Visited[graph.AdjList[currentVertex][i]])
                         {
-                            visited[adjList[curr][i]] = true;
-                            vertexList[adjList[curr][i]] = vertexList[curr] + 1;
-                            dfsqueue.Enqueue(adjList[curr][i]);
+                            graph.Visited[graph.AdjList[currentVertex][i]] = true;
+                            distanceFromStart[graph.AdjList[currentVertex][i]] = distanceFromStart[currentVertex] + 1;
+                            queue.Enqueue(graph.AdjList[currentVertex][i]);
                         }
                     }
                 }
             }
+
+            return distanceFromStart;
         }
     }
 }
